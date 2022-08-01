e8ba66859caf071f2a3a26fff558f37ee21cb7b7
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-08-01 13:27:36 -0500

    implemented products and departments classes

---
+++ ORMIntro/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMIntro
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@Name, @Price, @CategoryID);",
+                new { Name = name, Price = price, CategoryID = categoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM Products;");
+        }
+
+        public void UpdateProduct(int productId, string name, double price)
+        {
+            _connection.Execute("UPDATE Products SET Name = @Name, Price = @Price WHERE ProductID= @ProductID;",
+                new { Name = name, Price = price, ProductID = productId });
+        }
+    }
+}
--- ORMIntro/Program.cs
+++ ORMIntro/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using ORMIntro;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+
+//Departments
+var repo = new DepartmentRepository(conn);
+
+//repo.InsertDepartment("My new Department");
+
+var departments = repo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine(department.Name +"\n\n");  
+}
+
+// Products 
+var productRepo = new ProductRepository(conn);
+
+var products = productRepo.GetAllProducts();
+
+productRepo.UpdateProduct(4, "Changed Product", 100                                                                                                                                                );
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Product Id:{ product.ProductID}");
+    Console.WriteLine($"product name:{product.Name}");
+    Console.WriteLine($"product Price:{product.Price}");
+    Console.WriteLine($"product CategoryID: {product.CategoryID}\n\n");
+}
+
+// productRepo.CreateProduct("laptop", 999, 8);
+
